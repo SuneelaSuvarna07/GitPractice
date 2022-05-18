62552664d764e2a7838e530492ad6352636332a8
suneela <sunilsuvarna87@gmail.com>
2022-05-18 11:53:35 +0530

    develobranch commit

---
+++ src/p3/GitFirstProject.java
@@ -0,0 +1,24 @@
+package p3;
+
+
+
+public class GitFirstProject {
+
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+			test();
+		
+	}
+	
+	public static void test()
+	{
+		System.out.println("test method");
+	}
+	
+	public static void disp()
+	{
+		System.out.println("disp method");
+	}
+
+}
---
+++ src/p3/SecondClass.java
@@ -0,0 +1,19 @@
+package p3;
+
+
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+			test();
+		
+	}
+	
+	public static void test()
+	{
+		System.out.println("test method");
+	}
+
+}
